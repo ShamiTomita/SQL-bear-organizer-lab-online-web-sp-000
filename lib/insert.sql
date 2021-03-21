23c87e299b94f5ee0730a9ab41e5f68f623685bb
Shami Tomita <shamitomita@gmail.com>
2021-03-21 21:21:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,16 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
 ("Mr. Chocolate")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate")
